d9fe42ef244f3a77044255ce22c92e69af327071
Rahul Khanal <rahul39khanal@gmail.com>
2023-10-25 10:30:35 +0545

    404 setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navigation from "./components/navigation-bar/nav";
+import { Route, Routes } from "react-router-dom";
+import PageNotFound from "./pages/404";
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="*" Component={PageNotFound}></Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/404.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      <h1>404 Error | Page not found</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
